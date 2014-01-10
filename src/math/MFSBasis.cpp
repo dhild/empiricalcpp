f0b4cd7f1783d9a882be451371e994d025c8e661
D. Ryan Hild <d.ryan.hild@gmail.com>
2014-01-09 22:10:29 -0700

    Renamed the math library files and started implementing the MFSBasis2D class.

--- src/math/MFSBasis.cpp
+++ src/math/MFSBasis.cpp
@@ -1,42 +1,33 @@
 #include <cmath>
 #include <complex>
-#include "basis.h"
+#include <Empirical/Basis>
+#include <boost/math/special_functions/hankel.hpp>
 
 using namespace empirical;
 using namespace Eigen;
 using namespace std;
 
-
-template<typename Scalar>
-complex<Scalar> evaluateHankel(const Scalar k, const Scalar r) {
+cScalar evaluateBasis(const Scalar k, const Scalar r) {
     if (k == 0) {
         return -log(r) / (2.0 * PI);
     }
 
-    return complex<Scalar>(0.25j) * hankel1(0, k * r);
+    return cScalar(0.25j) * boost::math::cyl_hankel_1(0, k * r);
 }
 
-template<typename Scalar>
-template<typename evalScalar>
-complex<evalScalar> MFSBasis2D<Scalar>::operator()(const evalScalar x, const evalScalar y) const {
-
+cScalar MFSBasis2D::operator()(const Scalar x, const Scalar y) const {
+    return
 }
 
-template<typename Scalar>
-template<typename evalScalar>
-complex<evalScalar> MFSBasis2D<Scalar>::operator()(const Array<evalScalar, Dynamic, 2> xy) const {
-}
+/** Gets the result of applying this basis function to the given coordinates. */
+cScalar MFSBasis2D::operator()(const Eigen::Array<Scalar, Eigen::Dynamic, 2> xy) const {}
 
-template<typename Scalar>
-template<typename cScalar>
-Matrix<complex<cScalar>, Dynamic, Dynamic> MFSBasis2D<Scalar>::operator()(
-        const Matrix<complex<cScalar>, Dynamic, Dynamic> complexPoints) const {
-}
-
-template<typename Scalar>
-complex<Scalar> hankel1(const complex<Scalar> v, const Scalar z) {
-    return 1.0;
-}
+/** For this operator, the matrix values are interpreted as points in the z plane.
+ *
+ * z = x + iy
+ */
+Eigen::Matrix<cScalar, Eigen::Dynamic, Eigen::Dynamic> MFSBasis2D::operator()(
+        const Eigen::Matrix<cScalar, Eigen::Dynamic, Eigen::Dynamic> complexPoints) const {}
 
 //complex MFSBasis::evaluate(const double k, const double px, const double py, const double qx, const double qy) const {
 //    const double dx = px - qx;
